db4f7b972b883771ea5cf221f911ebf545f8d28e
Shubham Sharma <shrma.shubham@protonmail.com>
2021-09-19 17:06:58 +0530

    added SASS

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import "./App.css";
+import "./components/sass/style.css";
 import Flexbox from "./components/flexbox/Flexbox";
 import ClickCounter from "./components/HOC/ClickCounter";
 import CommentCounter from "./components/HOC/CommentCounter";
+import Sass from "./components/sass/Sass";
 import { ContextProvider } from "./context/DemoContext";
 
 const App = () => {
   return (
     <ContextProvider>
       <div>
-        <Flexbox />
+        <Sass />
       </div>
     </ContextProvider>
   );
---
+++ src/components/sass/Sass.js
@@ -0,0 +1,16 @@
+import React from "react";
+const Sass = () => {
+  return (
+    <>
+      <div className="flex-container ">
+        <div className="flex-itemm">XYZ</div>
+        <div className="flex-itemm">is</div>
+        <div className="flex-itemm">a</div>
+        <div className="flex-itemm">React</div>
+        <div className="flex-itemm">Developer</div>
+      </div>
+    </>
+  );
+};
+
+export default Sass;
