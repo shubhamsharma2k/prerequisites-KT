8fad6705a866840fa0dd981a1b01ae3c69af540e
Shubham Sharma <shrma.shubham@protonmail.com>
2021-09-19 19:02:01 +0530

    some Lodash methods

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import "./App.css";
 import "./components/sass/style.css";
-import Flexbox from "./components/flexbox/Flexbox";
-import ClickCounter from "./components/HOC/ClickCounter";
-import CommentCounter from "./components/HOC/CommentCounter";
-import Sass from "./components/sass/Sass";
 import { ContextProvider } from "./context/DemoContext";
+import DemoLodash from "./components/DemoLodash";
 
 const App = () => {
   return (
     <ContextProvider>
       <div>
-        <Sass />
+        <DemoLodash />
       </div>
     </ContextProvider>
   );
--- src/components/DemoLodash.js
+++ src/components/DemoLodash.js
@@ -1,9 +1,42 @@
+import _ from "lodash";
 import React from "react";
 
 const DemoLodash = () => {
   //ARRAY FUNCTIONS
 
-  return <div>LODASH:</div>;
+  var users = [
+    { user: "barney", age: 36, active: true },
+    { user: "fred", age: 40, active: false },
+    { user: "Shubham", age: 23, active: true },
+    { user: "Karan", age: 24, active: false },
+    { user: "Vij", age: 30, active: false },
+    { user: "Kusha", age: 50, active: true },
+  ];
+
+  var emptyUser = {};
+
+  //Filter Method _.filter
+  console.log(
+    _.filter(users, (o) => {
+      return o.age >= 40;
+    })
+  );
+
+  //Sum Method _.sum
+  console.log("_.sum() = " + _.sum([1, 2, 3, 4, 5]));
+
+  //isEmpty Method _.isEmpty
+  console.log("_.isEmpty() " + _.isEmpty(emptyUser));
+
+  //find Method _.find
+  console.log(
+    "_.find() " +
+      _.find(users, (o) => {
+        return o.age < 40;
+      })
+  );
+
+  return <div>LODASH: See Console.log</div>;
 };
 
 export default DemoLodash;
